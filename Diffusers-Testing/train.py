4e9fef4278b06d0e88f724708f1ab3702ca5e5b9
Talha Ahmed <24100033@lums.edu.pk>
2025-04-20 18:57:12 +0500

    Auto-commit: و 18:57:12 PKT ت 20 اپریل 2025

--- Diffusers-Testing/train.py
+++ Diffusers-Testing/train.py
@@ -1,5 +1,6 @@
 import argparse, logging, time, torch
 from architectures import LDM_Segmentor
+from diffusers import DDPMScheduler
 from torch.amp import autocast, GradScaler
 from torch.nn.functional import l1_loss
 from torch.optim import AdamW
@@ -17,8 +18,8 @@ def trainer(model, optimizer, train_loader, device, scheduler, scaler, val_loade
         model.train()
         optimizer.zero_grad()
         for step, batch in enumerate(train_loader):
-            image, mask = batch['aug_image'], batch['aug_mask']
-            timesteps = torch.randint(0, scheduler.num_train_timesteps, (len(train_loader),), device = device).long()
+            image, mask = batch['aug_image'].half(), batch['aug_mask'].half()
+            timesteps = torch.randint(0, scheduler.config.num_train_timesteps, (len(train_loader),), device = device).long()
 
             with autocast(device, enabled=True):
                 output = model(image, mask, timesteps)
@@ -33,20 +34,20 @@ def trainer(model, optimizer, train_loader, device, scheduler, scaler, val_loade
         train_losses.append(epoch_loss / len(train_loader))
         print(f'epoch {epoch + 1} train_loss: {train_losses[-1]}')
         if (epoch + 1) % VAL_INTERVAL == 0:
-            val_losses.append(validator(model, val_loader, device))
+            val_losses.append(validator(model, val_loader, scheduler, device))
             print(f'epoch {epoch + 1} val_loss: {val_losses[-1]}')
         
         print(f'ending epoch {epoch + 1}.')
         
     return train_losses, val_losses
 
-def validator(model, val_loader, device):
+def validator(model, val_loader, scheduler, device):
     epoch_loss = 0
     model.eval()
     with torch.no_grad():
         for step, batch in enumerate(val_loader):
             image, mask = batch['aug_image'], batch['aug_mask']
-            timesteps = torch.randint(0, scheduler.num_train_timesteps, (len(val_loader),), device = device).long()
+            timesteps = torch.randint(0, scheduler.config.num_train_timesteps, (len(val_loader),), device = device).long()
 
             with autocast(device, enabled=True):
                 output = model(image, mask, timesteps)
@@ -63,6 +64,7 @@ def main():
 
     model = LDM_Segmentor().to(device)
     optimizer = AdamW(model.parameters(), lr=LR, betas=(0.9, 0.999), weight_decay=0.0001)
+    scheduler = DDPMScheduler(num_train_timesteps=NUM_TRAIN_TIMESTEPS)
     scaler = GradScaler(device)
     train_loader = get_dataloaders(
                 BASE_DIR, split_ratio = SPLIT_RATIOS, split = 'train',
@@ -75,7 +77,7 @@ def main():
 
     print('starting training.')
     start_time   = time.time()
-    losses = trainer(model, optimizer, train_loader, device, scaler, val_loader)      
+    losses = trainer(model, optimizer, train_loader, device, scheduler, scaler, val_loader)      
     print(f'execution time: {(time.time() - start_time) // 60.0} minutes')
 
 if __name__ == '__main__':
